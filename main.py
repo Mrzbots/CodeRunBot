dd04fbf23e2b0e392492e87459536915f8821e5f
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-09-21 19:06:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from settings import *
 from utils.message import *
 from pyrogram.types import *
 
-bot = Client("my_bot", api_id=27215224, api_hash="688ae67db37f0ae991c3ecb97d73ff0a", bot_token="7851237202:AAEszK9R4Sr99thXEiQnFbV8K4nFu9Ksjew")
+bot = Client("bot", api_id=27215224, api_hash="688ae67db37f0ae991c3ecb97d73ff0a", bot_token="7851237202:AAEszK9R4Sr99thXEiQnFbV8K4nFu9Ksjew")
 
 @bot.on_message(filters.command("run"))
 async def run(client, message):
