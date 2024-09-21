3ebe2d42b369424c5c072369b53e0b44594d689f
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-09-21 19:02:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent
 bot = Client("my_bot", api_id=27215224, api_hash="688ae67db37f0ae991c3ecb97d73ff0a", bot_token="7851237202:AAEszK9R4Sr99thXEiQnFbV8K4nFu9Ksjew")
 
 @bot.on_message(filters.command("run"))
-async def ru(client, message):
+async def run(client, message):
     lang, code = message.text.split(maxsplit=2)[1:]
     request = RunRequest(lang, code)
     response = execute_code(request)   
