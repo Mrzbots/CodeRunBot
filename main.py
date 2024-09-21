9d8d3d590eed326a9a9425ebd21c11887919d55b
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-09-21 16:49:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ bot = Client("my_bot", api_id=27215224, api_hash="688ae67db37f0ae991c3ecb97d73ff
 async def run_code(client, message):
     lang, code = message.text.split(maxsplit=2)[1:]
     request = RunRequest(lang, code)
-    response = run_code(request, message)   
+    response = await run_code(request, message)   
     await message.reply(f"Output: {response}")
     
 @bot.on_inline_query(filters.regex(r"^(\w+)\s(.*)$"))
