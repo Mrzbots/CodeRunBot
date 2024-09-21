2a71d6cb914e63b48ce1b372e6ee3766237df713
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-09-21 19:04:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from settings import *
 from utils.message import *
-from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent
+from pyrogram.types import *
 
 bot = Client("my_bot", api_id=27215224, api_hash="688ae67db37f0ae991c3ecb97d73ff0a", bot_token="7851237202:AAEszK9R4Sr99thXEiQnFbV8K4nFu9Ksjew")
 
