8e6a01cda8248efd694422b15d03bfb796cd9253
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-09-21 19:56:11 +0530

    Delete bot directory

--- bot/langs.py
+++
@@ -1,7 +0,0 @@
-from settings import LANGS
-from main import bot
-from pyrogram import filters 
-
-@bot.on_message(filters.command("langs"))
-async def langs(client, message):
-    await message.reply_text(LANGS)
