8cc28ec73c107f78a0b2dda2845a06f68befe1e0
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-09-21 19:34:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ from pyrogram import Client, filters
 
 bot = Client("bot", api_id=27215224, api_hash="688ae67db37f0ae991c3ecb97d73ff0a", bot_token="7851237202:AAEszK9R4Sr99thXEiQnFbV8K4nFu9Ksjew", plugins=dict(root="bot"))
 
+bot.run()
