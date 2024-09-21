82513c26eda9d28ce1aa2d39aa2751afa14bd905
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-09-21 19:35:56 +0530

    Done

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from pyrogram import Client, filters
+import logging
+logging.basicConfig(level=logging.INFO)
 
 bot = Client("bot", api_id=27215224, api_hash="688ae67db37f0ae991c3ecb97d73ff0a", bot_token="7851237202:AAEszK9R4Sr99thXEiQnFbV8K4nFu9Ksjew", plugins=dict(root="bot"))
 
+print("Bot now working 🥰")
 bot.run()
