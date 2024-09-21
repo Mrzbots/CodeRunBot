10576aa7a2ffef063657cd289666d009e7e8a8aa
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-09-21 19:10:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,4 @@
 from pyrogram import Client, filters
-from settings import *
-from utils.message import *
-from pyrogram.types import *
 
 bot = Client("bot", api_id=27215224, api_hash="688ae67db37f0ae991c3ecb97d73ff0a", bot_token="7851237202:AAEszK9R4Sr99thXEiQnFbV8K4nFu9Ksjew", plugins=dict(root="bot"))
 
-@bot.on_message(filters.command("run"))
-async def run(client, message):
-    lang, code = message.text.split(maxsplit=2)[1:]
-    request = RunRequest(lang, code)
-    response = execute_code(request)   
-    await message.reply(f"Output: {response}")
-
-@bot.on_message(filters.command("langs"))
-async def langs(client, message):
-    await message.reply_text(LANGS)
-
-@bot.on_inline_query()
-async def inline(client, query):    
-    text = query.query
-    lang = text.split(maxsplit=1)[0]
-    code = text.split(maxsplit=1)[1]
-    request = RunRequest(lang, code)
-    response = execute_code(request)
-    await query.answer([
-        InlineQueryResultArticle(
-            title="Run Code",
-            description=f"Output: {response}",
-            input_message_content=InputTextMessageContent(f"Output: {response}")
-        )
-    ])
-           
