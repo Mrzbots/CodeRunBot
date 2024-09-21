7e421402db8d06cc84be3e7a3a1221ee9f34c3ca
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-09-21 18:54:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 from pyrogram import Client, filters
 from settings import *
+from utils.message import *
 from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent
 
 bot = Client("my_bot", api_id=27215224, api_hash="688ae67db37f0ae991c3ecb97d73ff0a", bot_token="7851237202:AAEszK9R4Sr99thXEiQnFbV8K4nFu9Ksjew")
 
 @bot.on_message(filters.command("run"))
-async def run_code_command(client, message):
+async def ru(client, message):
     lang, code = message.text.split(maxsplit=2)[1:]
     request = RunRequest(lang, code)
     response = execute_code(request)   
     await message.reply(f"Output: {response}")
 
+@bot.on_message(filters.command("langs"))
+async def langs(client, message):
+    await message.reply_text(LANGS)
+
 @bot.on_inline_query()
-async def inline_run_code(client, inline_query):    
+async def inline(client, inline_query):    
     text = inline_query.query
-    print(text)
     lang = text.split(maxsplit=1)[0]
-    print(lang)
     code = text.split(maxsplit=1)[1]
-    print(code)
     request = RunRequest(lang, code)
     response = execute_code(request)
     await inline_query.answer([
