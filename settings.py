8bf4aa201247a502e8c7f0e84ddc96307b268465
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-09-21 16:23:42 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,21 +1,12 @@
-from pymongo import MongoClient
 import requests
 import logging
-
 logging.basicConfig(level=logging.INFO)
 
 class RunRequest:
     def __init__(self, language: str, code: str, stdin: str):
         self.language = language
         self.code = code
-        self.stdin = stdin
-
-class RunResponse:
-    def __init__(self, result: str, output: str, compiler_output: str):
-        self.result = result
-        self.output = output
-        self.compiler_output = compiler_output
-
+         
 result_success = "success"
 result_error = "error"
 result_unknown = "unknown"
@@ -24,8 +15,7 @@ def run_code(request: RunRequest) -> RunResponse:
     json_body = {
         "language": request.language,
         "version": "*",
-        "files": request.code,
-        "stdin": request.stdin
+        "files": request.code,        
     }
     try:
         response = requests.post("https://horrid-api-yihb.onrender.com/execute", json=json_body)
@@ -35,15 +25,16 @@ def run_code(request: RunRequest) -> RunResponse:
         return RunResponse(result_unknown, "", "")
 
     if response.status_code != 200:
-        error_message = response.json().get("Message", "")
+        error_message = response.json().get("message", "")
         if not error_message:
             logging.error(response.text)
             return RunResponse(result_unknown, "", "")
         return RunResponse(result_error, error_message, "")
 
     data = response.json()
-    return RunResponse(result_success, data["run"]["output"])
+    lol = data['run']['output']
+    if lol.strip() != '':  
+        return RunResponse(lol)
+    else:
+        return RunResponse(result_success)
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client["piston"]
-stats_collection = db["stats"]
