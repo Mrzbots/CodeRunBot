7165c7b6aaaea68fa6ffc1db246a705e90a7b191
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-09-22 12:48:18 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -18,22 +18,6 @@ def execute_code(request: RunRequest):
         "files": request.code,        
     }
     try:
-        response = requests.post("https://horrid-api-yihb.onrender.com/execute", json=json_body)
-        response.raise_for_status()
-    except requests.RequestException as e:
-        logging.error(e)
-        return result_unknown, "", ""       
-
-    if response.status_code != 200:
-        error_message = response.json().get("message", "")
-        if not error_message:
-            logging.error(response.text)
-            return result_unknown, "", ""
-        return result_error, error_message, ""
-
-    data = response.json()
-    lol = data['run']['output']
-    if lol.strip() != '':  
-        return lol
-    else:
-        return result_success
+        response = requests.post("https://horrid-api-yihb.onrender.com/execute", json=json_body)               
+        data = response.json()                     
+        return data
