6f6a0b96e8b31a005bae629214b8b3a5ac621eb1
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-10-05 20:50:22 +0530

    Create start.py

---
+++ CodeRunBot/start.py
@@ -0,0 +1,99 @@
+# (c) @HORRIDduo
+
+from pyrogram import Client, filters
+from settings import *
+from utils.message import *
+from pyrogram.types import *
+
+
+def get_reply_markup(query):
+    buttons = [
+        [
+            InlineKeyboardButton('Run Code', switch_inline_query_current_chat=query)
+        ]
+    ]
+    return InlineKeyboardMarkup(buttons)
+
+
+@Client.on_message(filters.command("langs"))
+async def langs(client, message):
+    await message.reply_text(LANGS)
+
+@Client.on_message(filters.command("help"))
+async def help(client, message):
+    await message.reply_text(HELP)
+
+@Client.on_message(filters.command("start"))
+async def start(client, message):
+    buttons = [
+        [InlineKeyboardButton("Support Group", url="https://t.me/XBOTSUPPORTS")],
+        [InlineKeyboardButton("Source Code", url="https://github.com/Mrzbots/CodeRunBot")]
+    ]
+    await message.reply_text(START, reply_markup=InlineKeyboardMarkup(buttons))
+    
+@bot.on_inline_query()
+async def inline(client, query):
+    text = query.query
+    parts = text.split(maxsplit=1)
+    
+    if len(parts) != 2:
+        await query.answer([
+            InlineQueryResultArticle(
+                title="Bad Query",
+                description="Usage: @GoodCodeRunBot [language] [code]",
+                input_message_content=InputTextMessageContent(HOW_INLINE)
+            )
+        ])
+        return
+    
+    lang, code = parts
+    
+    if not lang:
+        await query.answer([
+            InlineQueryResultArticle(
+                title="Bad Query",
+                description="Usage: @GoodCodeRunBot [language] [code]",
+                input_message_content=InputTextMessageContent(HOW_INLINE)
+            )
+        ])
+        return
+    
+    if not code:
+        await query.answer([
+            InlineQueryResultArticle(
+                title="Bad Query",
+                description="Usage: @GoodCodeRunBot [language] [code]",
+                input_message_content=InputTextMessageContent(HOW_INLINE)
+            )
+        ])
+        return
+    
+    request = RunRequest(lang, code)
+    response = execute_code(request)   
+    reply_markup = get_reply_markup(text)
+    
+    if 'run' in response and 'output' in response['run']:
+        data = response["run"]["output"] 
+        if data.strip() != '':  
+            res = data
+        else:
+            res = result_success
+        await query.answer([
+            InlineQueryResultArticle(
+                title="Output",
+                description=f"{res}",
+                reply_markup=reply_markup,
+                input_message_content=InputTextMessageContent(OUTPUT.format(response["language"], response["version"], code, res))
+            )
+        ])
+    else:
+        await query.answer([
+            InlineQueryResultArticle(
+                title="Unknown Language",
+                description="Unknown language",              
+                input_message_content=InputTextMessageContent("Hey, your language is unknown. Maybe it's a spelling mistake? If you want to see the supported languages, use the. /langs command")
+            )
+        ])
+        
+
+
