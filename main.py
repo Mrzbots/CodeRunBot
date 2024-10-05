609ecf3a56ccf7e17a06e6c18cb30860d8c38f29
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-10-05 20:44:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-# code by @HORRIDduo
+# (c) @HORRIDduo
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+
+
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            name="Coderunbot",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=200,
+            plugins={"root": "CodeRunBot"},
+            sleep_threshold=15,
+        )
+
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        print(f"{me.first_name} Now Working 😘")
+        
+Bot().run()
 
 from pyrogram import Client, filters
 from settings import *
